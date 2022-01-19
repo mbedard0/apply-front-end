4c51efa24e3e7e378861ff8497249d75cb43585b
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-18 19:23:00 -0500

    stub home/landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,19 @@
-import styles from './Landing.module.css'
+// import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+    <main>
+      <div className="text-center text-neutral-content">
+        <h1 className="mb-5 text-5xl font-bold text-black my-5">
+          Apply
+        </h1>
+        <div className="form-control flex justify-center">
+          <input type="text" placeholder="Find Jobs" className="input input-bordered text-black" />
+        </div>
+        <button className="btn btn-primary my-5">Search</button>
+      </div>
+      <div className="divider"></div>
+      {/* about us or what we do.... even the github stuff maybe can go here? */}
     </main>
   )
 }
