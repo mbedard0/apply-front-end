7f9286bd4bc84d355477754a11a190a64e380c44
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-20 13:10:59 -0500

    stub user profile in nav and add profile state

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -9,4 +9,11 @@ async function getAllProfiles() {
   return await res.json()
 }
 
-export { getAllProfiles }
+function getMyProfile(id) {
+  return fetch(`${BASE_URL}/${id}`, {
+    headers: { Authorization: `Bearer ${tokenService.getToken()}` }
+  })
+    .then(res => res.json())
+}
+
+export { getAllProfiles, getMyProfile }
