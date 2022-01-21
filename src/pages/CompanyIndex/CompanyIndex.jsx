b5db6bd791709704734fe6e6521fc7f036ff931a
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-21 09:05:27 -0500

    fix reload for useeffect

--- src/pages/CompanyIndex/CompanyIndex.jsx
+++ src/pages/CompanyIndex/CompanyIndex.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import * as companyService from '../../services/companyService'
 
 const CompanyIndex = props => {
@@ -8,7 +8,7 @@ const CompanyIndex = props => {
   useEffect(() => {
     companyService.getCompanies()
     .then(allCompanies => setCompanies(allCompanies))
-  },[companies])
+  },[])
 
   return (
     <>
@@ -20,7 +20,12 @@ const CompanyIndex = props => {
           return (
             <div class="card w-4/5 place-content-center card-bordered card-compact lg:card-normal">
               <div class="card-body">
-                <h2 class="card-title link link-neutral">{company.companyName}</h2>
+                <Link
+                  to='/companyDetails'
+                  state={company}
+                >
+                  <h2 class="card-title link link-neutral">{company.companyName}</h2>
+                </Link>
                 <h3 className="text-gray-500 mb-2 -mt-1 italic">{company.location}</h3>
                 <p>{company.description}</p>
               </div>
