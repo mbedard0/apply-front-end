26656dfbcd970ccd0b7605b648f6623fbdbcb613
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-25 16:28:25 -0500

    update conditional rendering for profile

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -8,7 +8,6 @@ const UserProfile = (props) => {
 
    useEffect(() => {
       if (profile && profile.status === 'company') {
-         // temporary fix until controller function is fixed
          profileService.getCompany(profile.company[0])
             .then(company => {
                setCompany(company)
@@ -18,62 +17,57 @@ const UserProfile = (props) => {
       }
    }, [profile])
 
-   if (company === undefined && profile === undefined) {
+   if (profile === undefined) {
       return (
          <>
             <h1 className="text-5xl flex justify-center">Loading Page...</h1>
          </>
       )
-   } else {
+   } else if (profile && company === undefined){
       return (
          <>
-            {profile.status === 'individual' ?
-               <>
-                  <main className="text-center">
-                     {/* depending on what we want, the information below can change */}
-                     <h1 className="text-5xl font-bold">{profile.name}</h1>
-                     <p>{profile.email}</p>
-                     <p>{profile.status}</p>
-                     {/* add change email? */}
-                     {/* incorporate the change password here as well*/}
+         <main className="text-center">
+            {/* depending on what we want, the information below can change */}
+            <h1 className="text-5xl font-bold">{profile.name}</h1>
+            <p>{profile.email}</p>
+            <p>{profile.status}</p>
+            {/* add change email? */}
+            {/* incorporate the change password here as well*/}
 
-                     <div>
-                        {/* Companies Applied For  */}
-                        <h2 className="text-4xl font-bold">Applications:</h2>
-                     </div>
+            <div>
+               {/* Companies Applied For  */}
+               <h2 className="text-4xl font-bold">Applications:</h2>
+            </div>
 
-                  </main>
-               </>
-               :
-               <>
-                  <main className="text-center">
-                     <div>
-                        {/* put image of company brand here  */}
-                        <div className="avatar placeholder">
-                           <div className="bg-neutral-focus text-neutral-content rounded-full w-24 h-24">
-                              <span className="text-xl">{company.companyName[0]}</span>
-                           </div>
-                        </div>
-                        <h1 className="text-5xl font-bold">Company Name: {company.companyName}</h1>
-                        <h1 className="text-3xl font-bold">Company Owner: {profile.name}</h1>
-                        <p>Company Email: {profile.email}</p>
-                        {/* make sure status is correct */}
-                        <p>{profile.status}</p>
-
-                        {/* company details  */}
-                        <p>{company.description}</p>
-                        <p>Size: {company.size}</p>
-                     </div>
-                     <div>
-                        {/* job listings  */}
-                        <h2 className="text-4xl font-bold">Your Job Listings:</h2>
-                     </div>
-                  </main>
-               </>
-            }
-         </>
-      );
-   }
+         </main>
+      </>
+      )
+      } else {
+         return (
+            <main className="text-center">
+            <div>
+               {/* put image of company brand here  */}
+               <div className="avatar placeholder">
+                  <div className="bg-neutral-focus text-neutral-content rounded-full w-24 h-24">
+                     <span className="text-xl">{company.companyName[0]}</span>
+                  </div>
+               </div>
+               <h1 className="text-5xl font-bold">Company Name: {company.companyName}</h1>
+               <h1 className="text-3xl font-bold">Company Owner: {profile.name}</h1>
+               <p>Company Email: {profile.email}</p>
+               {/* make sure status is correct */}
+               <p>{profile.status}</p>
+               {/* company details  */}
+               <p>{company.description}</p>
+               <p>Size: {company.size}</p>
+            </div>
+            <div>
+               {/* job listings  */}
+               <h2 className="text-4xl font-bold">Your Job Listings:</h2>
+            </div>
+         </main>
+         )
+      }
 }
 
 export default UserProfile;
